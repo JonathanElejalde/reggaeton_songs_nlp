79cdc2ba4d3e286fcdb885d44679e2e284bac621
Jonathan Elejalde <jonathanelejalde@hotmail.com>
2021-01-05 13:43:01 -0500

    Solved syntax error

--- preprocessing/text_cleaning_spelling.py
+++ preprocessing/text_cleaning_spelling.py
@@ -142,6 +142,9 @@ def spell_checking(lyrics, spellchecker):
     Takes the song lyrics and 
     correct misspelling words in the lyrics
     using an SpellChecker instance
+    Args:
+        lyrics: str
+        spellchecker: SpellChecker(language='es') instance
     """
 
     # We are going to check spelling by line,
@@ -182,11 +185,12 @@ def spell_checking(lyrics, spellchecker):
 
 
 if __name__ == "__main__":
-    data_path = "..\data\lyrics_labeled.csv"
+    pass
+    # data_path = "..\data\lyrics_labeled.csv"
 
-    songs = pd.read_csv(data_path)
+    # songs = pd.read_csv(data_path)
 
-    prueba = songs[:15]
+    # prueba = songs[:15]
 
     # prueba["lyrics"] = prueba.lyrics.apply(lambda x: remove_text(x))
 
@@ -198,11 +202,11 @@ if __name__ == "__main__":
     # l = remove_short_lines(l)
 
     # print(l)
-    song1 = "Bebé, Real Hasta La Muerte, baby\r\nAnuel\r\nW en conexión con Anuel (uah)\r\n\r\n[Anuel AA y Wisin]\r\nPensando en ti, pensando en mí (oah)\r\nYo perdí'o en tu silueta (Real Hasta La Muerte)\r\nTú encima 'e mí, yo encima 'e ti (oah)\r\nDevorándote completa (duro)\r\n\r\nPensando en ti, pensando en mí (uah, uah)\r\nTú mojándote en mi cama inquieta\r\nYo encima 'e ti, yo adentro 'e ti\r\nMientra' tú gritas y me aprieta' (señore', Doble U)\r\n\r\nDesde que te vi en el caserío supe que eso sería mío\r\nNos vimos en la disco y se formó un desafío\r\nNunca me olvido de ese día, los dos bien amanecío's\r\nTú estabas a fuego y yo seguía prendí'o\r\nEres atrevida, el amor de mi vida\r\nSon veinticinco en su corrillo y ella es la más temida\r\nVamos a vernos de nuevo y que nada lo impida\r\nTú sabes que hay muchas pero tú eres mi preferida\r\nTú me tiene' viviendo de los recuerdo'\r\nY ese totito yo te lo muerdo, uh, yeh\r\nYo quiero comerte, uah\r\nHasta la muerte, uah\r\n\r\n[Anuel AA]\r\nPensando en ti, pensando en mí\r\nYo perdí'o en tu silueta\r\nTú encima 'e mí, yo encima 'e ti (oah)\r\nDevorándote completa\r\n\r\nPensando en ti, pensando en mí (uah, uah)\r\nTú mojándote en mi cama inquieta\r\nYo encima 'e ti, yo adentro 'e ti\r\nMientra' tú gritas y me aprieta'\r\n\r\n[Wisin y Anuel AA]\r\nSigo preso en tu recuerdo\r\nTu olor aún siento en la habitación (W en conexión con Anuel)\r\nAquellas noches no consigo olvidar (yeh, eh)\r\nCierro mis ojos y te imagino aquí (Multimillo Records)\r\nMi cama pregunta por ti (Real Hasta La Muerte)\r\nQuisiera volverte a tocar (duro)\r\nRomeo y Julieta\r\nSin los condones y con la glopeta, 35, 000 en mi roleta\r\nBebecita, yo me vengo cuando tú grita\r\nY tú te viene cuando tu totito te palpita\r\n\r\nPensando en ti, pensando en mi\r\nYo adentro 'e tu cuerpo\r\nTú encima 'e mí, yo adentro e' ti\r\nTú ere' un oasis en el desierto\r\n\r\nTú me tienes viviendo de los recuerdos\r\nY ese cuellito yo te lo muerdo, uh, yeh\r\nYo quiero comerte, uah\r\nHasta la muerte, uah\r\n\r\n[Anuel AA]\r\nPensando en ti, pensando en mi\r\nYo perdí'o en tu silueta\r\nTú encima 'e mí, yo encima 'e ti\r\nDevorándote completa\r\n\r\nPensando en ti, pensando en mí\r\nTú mojándote en mi cama inquieta\r\nYo encima 'e ti, yo adentro 'e ti\r\nMientra' tú gritas y me aprieta'\r\n\r\n[Wisin y Anuel AA]\r\nBaby no disimules que tú te pones nerviosa cuando llegamo'\r\nCanciones como esta a muchos le dan alegría\r\nA otros le dan dolor de barriga\r\nJajajajaja\r\nChris Jeday, Gaby Music, Anuel, W\r\nSeñores; La Champions League\r\n(Real Hasta La Muerte, baby)\r\n¿Oí'te?\r\nLa Champions League\r\n(Uah)"
+    # song1 = "Bebé, Real Hasta La Muerte, baby\r\nAnuel\r\nW en conexión con Anuel (uah)\r\n\r\n[Anuel AA y Wisin]\r\nPensando en ti, pensando en mí (oah)\r\nYo perdí'o en tu silueta (Real Hasta La Muerte)\r\nTú encima 'e mí, yo encima 'e ti (oah)\r\nDevorándote completa (duro)\r\n\r\nPensando en ti, pensando en mí (uah, uah)\r\nTú mojándote en mi cama inquieta\r\nYo encima 'e ti, yo adentro 'e ti\r\nMientra' tú gritas y me aprieta' (señore', Doble U)\r\n\r\nDesde que te vi en el caserío supe que eso sería mío\r\nNos vimos en la disco y se formó un desafío\r\nNunca me olvido de ese día, los dos bien amanecío's\r\nTú estabas a fuego y yo seguía prendí'o\r\nEres atrevida, el amor de mi vida\r\nSon veinticinco en su corrillo y ella es la más temida\r\nVamos a vernos de nuevo y que nada lo impida\r\nTú sabes que hay muchas pero tú eres mi preferida\r\nTú me tiene' viviendo de los recuerdo'\r\nY ese totito yo te lo muerdo, uh, yeh\r\nYo quiero comerte, uah\r\nHasta la muerte, uah\r\n\r\n[Anuel AA]\r\nPensando en ti, pensando en mí\r\nYo perdí'o en tu silueta\r\nTú encima 'e mí, yo encima 'e ti (oah)\r\nDevorándote completa\r\n\r\nPensando en ti, pensando en mí (uah, uah)\r\nTú mojándote en mi cama inquieta\r\nYo encima 'e ti, yo adentro 'e ti\r\nMientra' tú gritas y me aprieta'\r\n\r\n[Wisin y Anuel AA]\r\nSigo preso en tu recuerdo\r\nTu olor aún siento en la habitación (W en conexión con Anuel)\r\nAquellas noches no consigo olvidar (yeh, eh)\r\nCierro mis ojos y te imagino aquí (Multimillo Records)\r\nMi cama pregunta por ti (Real Hasta La Muerte)\r\nQuisiera volverte a tocar (duro)\r\nRomeo y Julieta\r\nSin los condones y con la glopeta, 35, 000 en mi roleta\r\nBebecita, yo me vengo cuando tú grita\r\nY tú te viene cuando tu totito te palpita\r\n\r\nPensando en ti, pensando en mi\r\nYo adentro 'e tu cuerpo\r\nTú encima 'e mí, yo adentro e' ti\r\nTú ere' un oasis en el desierto\r\n\r\nTú me tienes viviendo de los recuerdos\r\nY ese cuellito yo te lo muerdo, uh, yeh\r\nYo quiero comerte, uah\r\nHasta la muerte, uah\r\n\r\n[Anuel AA]\r\nPensando en ti, pensando en mi\r\nYo perdí'o en tu silueta\r\nTú encima 'e mí, yo encima 'e ti\r\nDevorándote completa\r\n\r\nPensando en ti, pensando en mí\r\nTú mojándote en mi cama inquieta\r\nYo encima 'e ti, yo adentro 'e ti\r\nMientra' tú gritas y me aprieta'\r\n\r\n[Wisin y Anuel AA]\r\nBaby no disimules que tú te pones nerviosa cuando llegamo'\r\nCanciones como esta a muchos le dan alegría\r\nA otros le dan dolor de barriga\r\nJajajajaja\r\nChris Jeday, Gaby Music, Anuel, W\r\nSeñores; La Champions League\r\n(Real Hasta La Muerte, baby)\r\n¿Oí'te?\r\nLa Champions League\r\n(Uah)"
 
-    spell = SpellChecker(language="es")
-    spell = add_artists(songs, spell)
-    lyrics = spell_checking(song1, spell)
+    # spell = SpellChecker(language="es")
+    # spell = add_artists(songs, spell)
+    # lyrics = spell_checking(song1, spell)
 
 # print(lyrics)
 
